af6832839010e8b41c3309b03f1e0d4b3a698d94
VladimirPopov23 <volodyaklever@gmail.com>
2024-12-01 18:47:10 +0300

    module_10_3.py
    01.12.2024 Задача "Банковские операции"

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
-# module_10_1.py
-# 28.11.2024 Задача "Потоковая запись в файлы"
+# module_10_3.py
+# 01.12.2024 Задача "Банковские операции"
 
-from time import sleep, time
-from threading import Thread
-
-time_start1 = time()
-
-
-def write_words(word_count, file_name):
-    with open(file_name, 'w', encoding='utf8') as file:
-        for i in range(word_count):
-            file.write(f'Какое-то слово № {i + 1}\n')
-            sleep(0.1)
-    print(f'Завершилась запись в файл {file_name}')
+import threading
+from random import randint
+import time
 
 
-write_words(10, 'example1.txt')
-write_words(30, 'example2.txt')
-write_words(200, 'example3.txt')
-write_words(100, 'example4.txt')
+class Bank:
+    def __init__(self):
+        self.balance = 0  # баланс банка (int)
+        self.lock = threading.Lock()  # объект класса Lock для блокировки потоков.
 
-time_end1 = time()
-print(f'Работа потоков {time_end1 - time_start1}')
+    def deposit(self):
+        for i in range(100):
+            refill = randint(50, 500)
+            self.balance += refill
+            print(f'Пополнение: {refill}. Баланс: {self.balance}')
+        if self.balance >= 500 and self.lock.locked() == True:
+            self.lock.release()
+        time.sleep(0.001)
+        return self.balance
 
-time_start2 = time()
+    def take(self):
+        for i in range(100):
+            cut = randint(50, 500)
+            print(f'Запрос на {cut}')
+            if cut <= self.balance:
+                self.balance -= cut
+                print(f'Снятие: {cut}. Баланс: {self.balance}')
+            else:
+                print(f'Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+                return self.balance
 
-thread5 = Thread(target=write_words, args=(10, 'example5.txt'))
-thread6 = Thread(target=write_words, args=(30, 'example6.txt'))
-thread7 = Thread(target=write_words, args=(200, 'example7.txt'))
-thread8 = Thread(target=write_words, args=(100, 'example8.txt'))
 
-thread5.start()
-thread6.start()
-thread7.start()
-thread8.start()
+bk = Bank()
 
-thread5.join()
-thread6.join()
-thread7.join()
-thread8.join()
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
 
-time_end2 = time()
-print(f'Работа потоков {time_end2 - time_start2}')
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
